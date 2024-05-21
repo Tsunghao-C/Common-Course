500ebf5e0b1195d5ead96a4d431e6617de9c25ab
Tsunghao <tsunghao.chen12@gmail.com>
2024-05-21 22:47:46 +0200

    revised strncmp to OK

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 09:39:18 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/05/21 20:28:46 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/05/21 22:46:40 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while ((i < (n - 1)) && (s1[i] || s2[i]) && (s1[i] != s2[i]))
+	while ((i < (n - 1)) && (s1[i] || s2[i]) && (s1[i] == s2[i]))
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
