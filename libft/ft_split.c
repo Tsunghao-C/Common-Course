2789596f53fd0ce06a0607081497984982d441af
Tsunghao Chen <tsuchen@made-f0Dr11s4.clusters.42paris.fr>
2024-05-24 10:54:46 +0200

    re-organised with strlcpy, added static, added notes

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,52 +6,11 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:13:36 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/05/22 17:01:17 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/05/24 10:54:04 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_count_subset(char const *s, char c)
-{
-	int		i;
-	int		count;
-	int		anchor;
-
-	i = -1;
-	count = 0;
-	anchor = -1;
-	while (s[++i])
-	{
-		if (s[i] == c)
-		{
-			if (i - anchor > 1)
-				count++;
-			anchor = i;
-		}
-	}
-	if (i - anchor > 1)
-		count++;
-	return (count);
-}
-
-static char	*ft_strchop(char const *s, int start, int end)
-{
-	char	*str;
-	int		i;
-
-	str = (char *)malloc((end - start) * sizeof(char));
-	if (!str)
-		return (0);
-	i = 0;
-	while (i < (end - start - 1))
-	{
-		str[i] = s[start + i + 1];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
 
 static void	ft_free_all(char **arr, int j)
 {
@@ -63,7 +22,18 @@ static void	ft_free_all(char **arr, int j)
 	free(arr);
 }
 
-int	ft_assign_arr(char **arr, char const *s, char c)
+static char	*ft_strchop(char const *s, int start, int end)
+{
+	char	*str;
+
+	str = (char *)malloc((end - start) * sizeof(char));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s + start + 1, end - start);
+	return (str);
+}
+
+static int	ft_assign_arr(char **arr, char const *s, char c, int s_len)
 {
 	int	i;
 	int	j;
@@ -72,7 +42,7 @@ int	ft_assign_arr(char **arr, char const *s, char c)
 	i = -1;
 	j = 0;
 	anchor = i;
-	while (++i <= (int)ft_strlen(s))
+	while (++i <= s_len)
 	{
 		if (s[i] == c || !s[i])
 		{
@@ -92,17 +62,47 @@ int	ft_assign_arr(char **arr, char const *s, char c)
 	return (1);
 }
 
+static int	ft_count_subset(char const *s, char c, int s_len)
+{
+	int	i;
+	int	cnt;
+	int	anchor;
+
+	i = -1;
+	cnt = 0;
+	anchor = i;
+	while (++i <= s_len)
+	{
+		if (s[i] == c || !s[i])
+		{
+			if (i - anchor > 1)
+				cnt++;
+			anchor = i;
+		}
+	}
+	return (cnt);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
+	int		s_len;
+	int		n_subset;
 
-	arr = (char **)malloc((ft_count_subset(s, c) + 1) * sizeof(char *));
+	s_len = (int)ft_strlen(s);
+	n_subset = ft_count_subset(s, c, s_len);
+	arr = (char **)malloc((n_subset + 1) * sizeof(char *));
 	if (!arr)
 		return (0);
-	if (ft_assign_arr(arr, s, c) == 0)
+	if (ft_assign_arr(arr, s, c, s_len) == 0)
 		return (0);
 	return (arr);
 }
+/* Note
+ * 1. if s is NULL, it will crash
+ * 2. if any of malloc fail, it will return NULL
+ * 3. if s is empty, it will return an empty arr with a NULL
+ */
 /*
 #include <stdio.h>
 #include <string.h>
