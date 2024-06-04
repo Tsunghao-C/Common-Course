ff30b8f0bbb3201b952d0c30a045b843ef1b0219
Tsunghao Chen <tsuchen@paul-f5Ar4s1.clusters.42paris.fr>
2024-06-04 15:35:52 +0200

    fixed Mandatory part leak issue

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 17:54:22 by tsuchen           #+#    #+#             */
+/*   Updated: 2024/06/04 15:33:02 by tsuchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdint.h>
+# include <errno.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# define ER STDERR_FILENO
+
+typedef struct s_list
+{
+	char			*str;
+	struct s_list	*next;
+}	t_list;
+
+void	ft_lst_append(t_list **lst, char *str);
+void	ft_update_list(t_list **lst);
+void	ft_delone(t_list *lst);
+int		ft_have_nl_lst(t_list *bgn_lst);
+int		ft_line_size(t_list *lst);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t siz);
+char	*get_next_line(int fd);
+char	*ft_gen_nl(t_list *lst);
+void	ft_fetch_nl(int fd, t_list **bgn_lst);
+
+#endif
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 16:25:45 by tsuchen           #+#    #+#             */
+/*   Updated: 2024/06/04 15:25:42 by tsuchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	ft_lst_append(t_list **lst, char *str)
+{
+	t_list	*new_lst;
+	t_list	*tmp;
+
+	new_lst = (t_list *)malloc(sizeof(t_list));
+	if (!new_lst)
+		return ;
+	new_lst->str = str;
+	new_lst->next = NULL;
+	if (!(*lst))
+	{
+		*lst = new_lst;
+		return ;
+	}
+	tmp = *lst;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new_lst;
+}
+
+int	ft_line_size(t_list *lst)
+{
+	int		len;
+	t_list	*tmp;
+	char	*tmp2;
+
+	len = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		tmp2 = tmp->str;
+		while (*tmp2)
+		{
+			if (*tmp2 == '\n')
+				return (++len);
+			len++;
+			tmp2++;
+		}
+		tmp = tmp->next;
+	}
+	return (len);
+}
+
+char	*ft_gen_nl(t_list *lst)
+{
+	char	*next_line;
+	char	*tmp2;
+	int		i;
+
+	if (ft_line_size(lst) == 0)
+		return (NULL);
+	next_line = (char *)malloc((ft_line_size(lst) + 1) * sizeof(char));
+	if (!next_line)
+		return (NULL);
+	i = 0;
+	while (lst)
+	{
+		tmp2 = lst->str;
+		while (*tmp2 && *tmp2 != '\n')
+			next_line[i++] = *tmp2++;
+		if (*tmp2 == '\n')
+		{
+			next_line[i++] = '\n';
+			break ;
+		}
+		lst = lst->next;
+	}
+	next_line[i] = '\0';
+	return (next_line);
+}
+
+void	ft_fetch_nl(int fd, t_list **bgn_lst)
+{
+	int		nu_rd;
+	char	*buff;
+
+	while (!ft_have_nl_lst(*bgn_lst))
+	{
+		buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buff)
+			return ;
+		nu_rd = read(fd, buff, BUFFER_SIZE);
+		if (!nu_rd || nu_rd == -1)
+		{
+			free(buff);
+			return ;
+		}
+		buff[nu_rd] = '\0';
+		ft_lst_append(bgn_lst, buff);
+	}
+}
+
+void	ft_update_list(t_list **lst)
+{
+	t_list	*tmp;
+	char	*str_left;
+	char	*tmp2;
+
+	tmp = *lst;
+	while ((*lst)->next)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		ft_delone(tmp);
+	}
+	tmp2 = (*lst)->str;
+	while (*tmp2 && *tmp2 != '\n')
+		tmp2++;
+	if (*tmp2 == '\n')
+		tmp2++;
+	str_left = (char *)malloc((ft_strlen(tmp2) + 1) * sizeof(char));
+	if (!str_left)
+		return ;
+	ft_strlcpy(str_left, tmp2, ft_strlen(tmp2) + 1);
+	ft_delone(*lst);
+	*lst = NULL;
+	ft_lst_append(lst, str_left);
+}
