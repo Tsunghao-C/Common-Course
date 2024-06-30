67d73ce2889f078fd602b6cb727dd702b997447d
Tsunghao <tsunghao.chen12@gmail.com>
2024-06-30 14:53:08 +0200

    add fractol

---
+++ fractol/includes/fractol.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 17:50:13 by tsuchen           #+#    #+#             */
+/*   Updated: 2024/06/28 18:04:32 by tsuchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../libft/includes/libft.h"
+# include "../libft/includes/ft_printf.h"
+# include "../libft/includes/get_next_line_bonus.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <limits.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <math.h>
+# include <complex.h>
+# include <../minilibx_opengl/mlx.h>
+# define OUT STDOUT_FILENO
+# define IN STDIN_FILENO
+# define ERR STDERR_FILENO
+# define P_NAME "fractol"
+
+#endif
---
+++ fractol/srcs/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 17:55:56 by tsuchen           #+#    #+#             */
+/*   Updated: 2024/06/28 18:06:19 by tsuchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	if (!mlx_win)
+		return (0);
+	mlx_loop(mlx);
+	return (0);
+}
