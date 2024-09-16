5b019442382bf3d90aa1dc09215454b0b4db7b90
Tsunghao <tsunghao.chen12@gmail.com>
2024-09-16 08:52:52 +0200

    ex05 started

---
+++ CPP_Module/Module01/ex05/Harl.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 18:47:17 by tsuchen           #+#    #+#             */
+/*   Updated: 2024/09/15 19:13:07 by tsuchen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+	private:
+		/* data */
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl(void);
+		~Harl(void);
+
+		void	complain(std::string level);
+};
+
+#endif
