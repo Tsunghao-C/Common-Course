1dfcbea4e2bd8975e6b5ad547a485c876cf50fef
Tsunghao <tsunghao.chen12@gmail.com>
2024-10-08 14:09:32 +0200

    reorganised main tests

--- CPP_Module/Module05/ex01/main.cpp
+++ CPP_Module/Module05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 10:53:10 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/09/26 18:09:12 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/10/08 14:09:03 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,19 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-	
-	try
-	{
-		Bureaucrat  tom("Tom", 20);
-		Form	doc1("Doc_1", 20, 30);
-		std::cout << doc1 << std::endl;
-		Form	doc2("Doc_2", 2, 30);
-		std::cout << tom << std::endl;
-		tom.signForm(doc1);
-		std::cout << doc1 << std::endl;
-		tom.increment();
-		std::cout << tom << std::endl;
-		tom.signForm(doc2);
-		tom.signForm(doc1);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Tom Error: " << e.what() << '\n';
+
+	{	
+	Bureaucrat  tom("Tom", 20);
+	Form	doc1("Doc_1", 20, 30);
+	std::cout << doc1 << std::endl;
+	Form	doc2("Doc_2", 2, 30);
+	std::cout << tom << std::endl;
+	tom.signForm(doc1);
+	std::cout << doc1 << std::endl;
+	tom.increment();
+	std::cout << tom << std::endl;
+	tom.signForm(doc2);
+	tom.signForm(doc1);
 	}
 	
 	try
