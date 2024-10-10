701a9bd606594f1200c6d6db4d3c1d8703759112
Tsunghao <tsunghao.chen12@gmail.com>
2024-10-10 13:04:07 +0200

    Fixed overflow of very big interger

--- CPP_Module/Module06/ex00/ScalarConverter_utils.cpp
+++ CPP_Module/Module06/ex00/ScalarConverter_utils.cpp
@@ -6,7 +6,7 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 11:17:47 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/10/10 12:46:43 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/10/10 13:01:05 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void ScalarConverter::do_nan(std::string const &s) {
 }
 
 void ScalarConverter::do_char(std::string const &s) {
-	// std::cout << "--------DO CHAR-----------" << std::endl;
+	std::cout << "--------DO CHAR-----------" << std::endl;
 	char	c = *s.begin();
 	std::cout << "Char: '" << c << "'" << std::endl;
 	std::cout << "Int: " << static_cast<int>(c) << std::endl;
@@ -41,7 +41,7 @@ void ScalarConverter::do_char(std::string const &s) {
 }
 
 void ScalarConverter::do_int(std::string const &s) {
-	// std::cout << "--------DO INT-----------" << std::endl;
+	std::cout << "--------DO INT-----------" << std::endl;
 	int		i = std::atoi(s.c_str());
 	if (i < 0 || i > 127) {
 		std::cout << "Char: " << "impossible" << std::endl;
@@ -56,7 +56,7 @@ void ScalarConverter::do_int(std::string const &s) {
 }
 
 void ScalarConverter::do_float(std::string const &s) {
-	// std::cout << "------DO FLOAT-------" << std::endl;
+	std::cout << "------DO FLOAT-------" << std::endl;
 	float	f = std::atof(s.c_str());
 	int		i = static_cast<int>(f);
 	if (i < 0 || i > 127) {
@@ -72,7 +72,7 @@ void ScalarConverter::do_float(std::string const &s) {
 }
 
 void ScalarConverter::do_double(std::string const &s) {
-	// std::cout << "------DO DOUBLE-------" << std::endl;
+	std::cout << "------DO DOUBLE-------" << std::endl;
 	double	d = std::atof(s.c_str());
 	int		i = static_cast<int>(d);
 	if (i < 0 || i > 127) {
@@ -88,7 +88,7 @@ void ScalarConverter::do_double(std::string const &s) {
 }
 
 void ScalarConverter::do_other(std::string const &s) {
-	// std::cout << "--------DO OTHER-----------" << std::endl;
+	std::cout << "--------DO OTHER-----------" << std::endl;
 	int		i = std::atoi(s.c_str());
 	if (i < 0 || i > 127) {
 		std::cout << "Char: " << "impossible" << std::endl;
@@ -97,7 +97,11 @@ void ScalarConverter::do_other(std::string const &s) {
 	} else {
 		std::cout << "Char: " << static_cast<char>(i) << std::endl;
 	}
-	std::cout << "Int: " << i << std::endl;
-	std::cout << "Float: " << std::fixed << std::setprecision(2) << static_cast<float>(i) << "f" << std::endl;
-	std::cout << "Double: " << static_cast<double>(i) << std::endl;
+	if (s != std::to_string(i)) {
+		std::cout << "Int: " << "impossible" << std::endl;
+	} else {
+		std::cout << "Int: " << i << std::endl;
+	}
+	std::cout << "Float: " << std::fixed << std::setprecision(2) << static_cast<float>(std::atof(s.c_str())) << "f" << std::endl;
+	std::cout << "Double: " << static_cast<double>(std::atof(s.c_str())) << std::endl;
 }
