7e5438e58a0e595d5d1e51e8336d39e723412237
Tsunghao <tsunghao.chen12@gmail.com>
2024-10-11 14:33:57 +0200

    adjusted main

--- CPP_Module/Module06/ex02/main.cpp
+++ CPP_Module/Module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsuchen <tsuchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 12:25:40 by tsuchen           #+#    #+#             */
-/*   Updated: 2024/10/11 12:38:48 by tsuchen          ###   ########.fr       */
+/*   Updated: 2024/10/11 14:33:25 by tsuchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,13 @@ void    identify(Base & p);
 
 int     main() {
     Base    *random1 = generate();
-    usleep(500);
-    Base    *random2 = generate();
-    usleep(500);
-    Base    *random3 = generate();
 
     std::cout << "Check using pointer" << std::endl;
     identify(random1);
-    identify(random2);
-    identify(random3);
 
     std::cout << "\nCheck using reference" << std::endl;
     identify(*random1);
-    identify(*random2);
-    identify(*random3);
 
     delete random1;
-    delete random2;
-    delete random3;
     return 0;
 }
